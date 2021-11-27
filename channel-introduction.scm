784c1e8df8692bf7a533366e3935c4ea70bf0ac2
ordog <ordogswayer@gmail.com>
2021-11-27 19:59:54 -0300

    add channel introduction

---
+++ channel-introduction.scm
@@ -0,0 +1,6 @@
+(channel
+  (name 't-guix-channels)
+  (url "https://github.com/Odoguchan/t-guix-channels")
+  (introduction
+   (make-channel-introduction
+    "395cac5cfff08172ba2d9763beb07453b945dc20")))
